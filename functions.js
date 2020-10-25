272304d6f22c8038a2c38d781c51cf37f9ee4d81
lmrabiah <lm.alrabiah@gmail.com>
2020-10-25 15:45:14 +0300

    it was working welll with console log but with return not

--- functions.js
+++ functions.js
@@ -7,10 +7,10 @@
  * greet("Michael") logs "Hello Michael"
  */
 function greet(name) {
-  // Your code here
+  console.log(`Hello ${name}`);
 }
 
-/**
+/**s
  * isOdd(n):
  * - receives a number n
  * - returns true if it's odd, false otherwise
@@ -18,36 +18,46 @@ function greet(name) {
  * e.g.
  * isOdd(7) -> true
  * isOdd(10) -> false
- */
+//  */
 function isOdd(n) {
-  // Your code here
+  if (n % 2 !== 0) {
+    return true;
+  }
+  return false;
 }
+isOdd(7);
 
-/**
- * oddsSmallerThan(n):
- * - receives a number n
- * - returns the number of ODD numbers smaller than n
- *
- * e.g.
- * oddsSmallerThan(7) -> 3
- * oddsSmallerThan(15) -> 7
- */
+// /**
+//  * oddsSmallerThan(n):
+//  * - receives a number n
+//  * - returns the number of ODD numbers smaller than n
+//  *
+//  * e.g.
+//  * oddsSmallerThan(7) -> 3
+//  * oddsSmallerThan(15) -> 7
+//  */
 function oddsSmallerThan(n) {
-  // Your code here
+  if (n % 2 !== 0) {
+    return (n - 1) / 2;
+  }
+  {
+    return n / 2;
+  }
 }
-
-/**
- * squareOrDouble(n):
- * - receives a number n
- * - returns its square if it's odd
- * - returns its double if it's even
- *
- * e.g.
- * squareOrDouble(16) -> 32
- * squareOrDouble(9) -> 81
- */
+oddsSmallerThan(7);
+// /**
+//  * squareOrDouble(n):
+//  * - receives a number ns
+//  * - returns its square if it's odd
+//  * - returns its double if it's even
+//  *
+//  * e.g.
+//  * squareOrDouble(16) -> 32
+//  * squareOrDouble(9) -> 81
+//  */
 function squareOrDouble(n) {
-  // Your code here
+  if (n % 2 === 0) return n * 2;
+  return n * n;
 }
 
-module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
+// module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
